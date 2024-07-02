9ed1694c1e7f9636830f6485e5a90a3d4a4e624b
Michael Birken <o__1@hotmail.com>
2024-07-01 22:38:21 -0400

    update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-
 import MainHeader from 'components/MainHeader.vue';
 import MainPage from 'components/MainPage.vue';
 import MainFooter from 'components/MainFooter.vue';
 </script>
 
 <template>
-  <q-layout view="hHh lpr fFf">
+  <q-layout view="hHh lpr fFf" style="overflow: hidden;">
 
     <MainHeader/>
 
