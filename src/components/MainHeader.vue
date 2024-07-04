e0eda3b4086f086bbf7613058bf767b2acb2dcc2
Michael Birken <o__1@hotmail.com>
2024-07-04 11:23:05 -0400

    update

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -4,14 +4,14 @@
 
 <template>
   <q-header>
-    <q-toolbar class="text-white" style="background: #1F1F1F;">
+    <q-toolbar class="text-white q-pl-none" style="background: #1F1F1F;">
       <div class="row justify-center" style="width: 170px;">
-        <q-btn class="q-mr-md" icon="play_circle" rounded color="secondary" no-caps label="Generate"/>
+        <q-btn class="q-mx-none" icon="play_circle" rounded color="green-6" no-caps label="Generate"/>
       </div>
-      <q-toolbar-title>
-        Weave Maze Generator
-      </q-toolbar-title>
+      <q-img src="white-logo.svg" spinner-color="white" style="width: 45px;"/>
+      <q-toolbar-title>Weave Maze Generator</q-toolbar-title>
       <div class="q-gutter-sm">
+        <q-btn icon="center_focus_strong" rounded color="primary" no-caps label="Center"/>
         <q-btn icon="interests" rounded color="primary" no-caps label="Mask"/>
         <q-btn icon="save" rounded color="primary" no-caps label="Save"/>
         <q-btn icon="print" rounded color="primary" no-caps label="Print"/>
