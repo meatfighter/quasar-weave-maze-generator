31d55b68d838bd48b2e9a27f62ac29b7864b1e7b
Michael Birken <o__1@hotmail.com>
2024-07-07 23:08:23 -0400

    update

--- src/components/MaskDialog.vue
+++ src/components/MaskDialog.vue
@@ -4,16 +4,22 @@ import { ref, watch } from 'vue';
 import { MAX_MAZE_SIZE } from 'src/app/controller/defaults';
 import { useOptionsStore } from 'stores/optionsStore';
 import { storeToRefs } from 'pinia';
+import { makeRgbasFromFile, makeRgbasFromUrl } from 'src/utils/images';
+import { useQuasar } from 'quasar';
 
 const IMAGE_FILE_EXTENSIONS = new Set<string>(
     ['apng', 'bmp', 'gif', 'ico', 'jpg', 'jpeg', 'svg', 'svgz', 'png', 'webp']);
+
+const $q = useQuasar();
 
 const optionsStore = useOptionsStore();
 const { maskRgbas } = storeToRefs(optionsStore);
 
 const dialogVisible = defineModel<boolean>();
-const imageUrl = ref('');
 
+const fileInput = ref<HTMLInputElement | null>(null);
+
+const imageUrl = ref('');
 watch(imageUrl, () => {
   let url = imageUrl.value;
   if (!url) {
@@ -27,8 +33,61 @@ watch(imageUrl, () => {
   if (index < 0 || index > url.length - 4 || !IMAGE_FILE_EXTENSIONS.has(url.substring(index + 1).toLowerCase())) {
     return;
   }
-  console.log(url);
+  makeRgbasFromUrl(url)
+    .then(rgbas => {
+      maskRgbas.value = rgbas;
+      closeDialog();
+    })
+    .catch(e => $q.notify({
+      message: (e as Error).message,
+      type: 'negative',
+      position: 'bottom',
+      closeBtn: true,
+    }));
 });
+
+function openFileBrowser() {
+  fileInput.value?.click();
+}
+
+function onFileChange(event: Event) {
+  onFiles((event.target as HTMLInputElement).files);
+}
+
+function onDrop(event: DragEvent) {
+  event.preventDefault();
+  if (!event.dataTransfer) {
+    return;
+  }
+  onFiles(event.dataTransfer.files);
+}
+
+function onFiles(files: FileList | null) {
+  if (!files || files.length === 0) {
+    return;
+  }
+  if (files.length !== 1) {
+    $q.notify({
+      message: 'Single files only.',
+      type: 'negative',
+      position: 'bottom',
+      closeBtn: true,
+    })
+    return;
+  }
+
+  makeRgbasFromFile(files[0])
+    .then(rgbas => {
+      maskRgbas.value = rgbas;
+      closeDialog();
+    })
+    .catch(e => $q.notify({
+      message: (e as Error).message,
+      type: 'negative',
+      position: 'bottom',
+      closeBtn: true,
+    }));
+}
 
 function removeMask() {
   maskRgbas.value = null;
@@ -36,12 +95,14 @@ function removeMask() {
 }
 
 function closeDialog() {
+  imageUrl.value = '';
   dialogVisible.value = false;
 }
 </script>
 
 <template>
-  <q-dialog :model-value="dialogVisible" @before-hide="closeDialog">
+  <q-dialog :model-value="dialogVisible" @before-hide="closeDialog" @drop.prevent="onDrop" @dragover.prevent
+            @dragenter.prevent>
     <q-card style="min-width: 50em;">
       <q-card-section class="q-pa-none" style="background: #2D2D2D;">
         <div class="row items-center justify-between q-pa-sm">
@@ -68,7 +129,9 @@ function closeDialog() {
         </div>
       </q-card-section>
       <q-card-section class="row justify-center">
-        <q-btn icon="drive_folder_upload" rounded no-caps color="primary" label="Or choose an image file"/>
+        <q-btn icon="drive_folder_upload" rounded no-caps color="primary" label="Or choose an image file"
+               @click="openFileBrowser"/>
+        <input type="file" ref="fileInput" @change="onFileChange" style="display: none" accept="image/*" />
       </q-card-section>
       <q-card-section>
         <q-input class="col" v-model="imageUrl" label="Or paste an image URL here" filled dense/>
