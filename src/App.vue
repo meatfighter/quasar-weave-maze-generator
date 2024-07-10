adfcc76a1bc9c586ed4e7d5aa2a4c917fe4220cd
Michael Birken <o__1@hotmail.com>
2024-07-09 21:37:31 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,4 @@ defineOptions({
 </template>
 
 <style>
-a,a:visited {
-  color: #58A6FF;
-  text-decoration: none;
-  white-space: nowrap;
-}
-
-a:hover,a:active {
-  color: #58A6FF;
-  text-decoration: underline;
-  white-space: nowrap;
-}
-
-input[type="number"] {
-  appearance: textfield;
-}
-
-input[type="number"]:focus,
-input[type="number"]:hover {
-  appearance: auto;
-}
 </style>
