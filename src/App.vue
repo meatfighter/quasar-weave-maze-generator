f7d468360294b3e4125718b4f317c2aeb485e7fc
Michael Birken <o__1@hotmail.com>
2024-07-14 13:25:26 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
 <script setup lang="ts">
+import { useRenderStore } from 'stores/renderStore';
+
+import { onMounted, onUnmounted } from 'vue';
+
 defineOptions({
   name: 'App'
+});
+
+const renderStore = useRenderStore();
+const { centerImage } = renderStore;
+
+let orientationType: OrientationType | undefined = undefined;
+
+const handleOrientationChange = () => {
+  if (window.screen.orientation.type !== orientationType) {
+    orientationType = window.screen.orientation.type;
+    setTimeout(centerImage, 250);
+  }
+};
+
+onMounted(() => {
+  if ('screen' in window && 'orientation' in window.screen) {
+    window.screen.orientation.addEventListener('change', handleOrientationChange);
+  }
+});
+
+onUnmounted(() => {
+  if ('screen' in window && 'orientation' in window.screen) {
+    window.screen.orientation.removeEventListener('change', handleOrientationChange);
+  }
 });
 </script>
 
